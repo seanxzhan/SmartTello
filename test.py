69454853d5fed6b127988b62caf68bcbc467d088
seanzhan0319 <seanzhan0319@hotmail.com>
2020-07-05 21:00:41 -0400

    Openpose is working with webcam now.

--- test.py
+++ test.py
@@ -1,11 +1,107 @@
+from imutils.video import VideoStream
+import numpy as np
+import argparse
+import imutils
+import time
 import cv2
-import sys
+import pygame
+from pygame.locals import *
 import os
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
-sys.path.append(dir_path + '/python/openpose/Release')
-os.environ['PATH'] = os.environ['PATH'] + ';' + dir_path + '/Release;' + dir_path + '/bin;'
+# constructing arguments and parsing arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-p", "--prototxt", required=True,
+                help="path to Caffe 'deploy' prototxt file")
+ap.add_argument("-m", "--model", required=True,
+                help="path to Caffe pre-trained model")
+ap.add_argument("-c", "--confidence", type=float, default=0.5,
+                help="minimum probability to filter weak detections")
+args = vars(ap.parse_args())
+#
+# print("[INFO] loading model...")
+# net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+#
+# # turning on webcam
+# print("[INFO] starting video stream...")
+webcam = VideoStream(src=0).start()
+time.sleep(2.0)
 
-import pyopenpose as openpose
 
-print("hello")
+class FrontEnd(object):
+
+    def __init__(self):
+        pygame.init()
+        self.screen = pygame.display.set_mode([960, 720])
+
+    def run(self):
+        showing = True
+        body_detection = False
+
+        while showing:
+            frame = webcam.read()
+            # cv2.imshow("Webcam", frame)
+            cam = webcam.frame
+            cv2.putText(cam, "body_detection: " + str(body_detection), 
+            (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 255, 255), 2)
+
+            for event in pygame.event.get():
+                if event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        showing = False
+                    elif event.key == K_b:
+                        if body_detection:
+                            body_detection = False
+                        else:
+                            body_detection = True
+            if not showing:
+                break
+
+            npoints = 15
+            POSE_PAIRS = [[0,1], [1,2], [2,3], [3,4], [1,5], [5,6], [6,7], 
+                [1,14], [14,8], [8,9], [9,10], [14,11], [11,12], [12,13]]
+
+            net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
+
+            # convert each frame into a blob
+            (height, width) = cam.shape[:2]
+            blob = cv2.dnn.blobFromImage(cv2.resize(cam, (300, 300)), 
+                1.0 / 255, (300, 300), (104.0, 177.0, 123.0))
+            net.setInput(blob)
+            output = net.forward()
+            
+            H = output.shape[2]
+            W = output.shape[3]
+            points = []
+            threshold = 0.1
+
+            for i in range(npoints):
+                probMap = output[0, i, :, :]
+                minVal, prob, minLoc, point = cv2.minMaxLoc(probMap)
+                x = (width * point[0]) / W
+                y = (height * point[1]) / H
+
+                if prob > threshold :
+                    cv2.circle(cam, (int(x), int(y)), 5, (0, 255, 255), thickness=-1, lineType=cv2.FILLED)
+                    cv2.putText(cam, "{}".format(i), (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 3, lineType=cv2.LINE_AA)
+                    points.append((int(x), int(y)))
+                else: 
+                    points.append(None)
+
+            for pair in POSE_PAIRS:
+                partA = pair[0]
+                partB = pair[1]
+
+                if points[partA] and points[partB]:
+                    cv2.line(cam, points[partA], points[partB], (0, 255, 255), 2)
+                    cv2.circle(cam, points[partA], 8, (0, 0, 255), thickness=-1, lineType=cv2.FILLED)
+            
+            cv2.imshow("Output", cam)
+
+
+def main():
+    frontend = FrontEnd()
+    frontend.run()
+
+
+if __name__ == '__main__':
+    main()
--- webcam.py
+++ webcam.py
@@ -1,14 +1,80 @@
-from imutils.video import VideoStream
-import numpy as np
-import argparse
-import imutils
-import time
 import cv2
-import pygame
-from pygame.locals import *
+import sys
 import os
+import time
+import math
+import numpy as py
+from sys import platform
+import argparse
+from imutils.video import VideoStream
 
-# constructing arguments and parsing arguments
+sys.path.append('C:\\Users\\seanz\\CS_Projects\\openpose\\build\\python\\openpose\\Release')
+os.environ['PATH'] = os.environ['PATH'] + ';' +\
+    'C:\\Users\\seanz\\CS_Projects\\openpose\\build\\x64\\Release' + ';' +\
+    'C:\\Users\\seanz\\CS_Projects\\openpose\\build\\bin'
+print(sys.path)
+
+import pyopenpose as op
+
+def set_params():
+        params = dict()
+        params["logging_level"] = 3
+        params["output_resolution"] = "-1x-1"
+        params["net_resolution"] = "-1x368"
+        params["model_pose"] = "BODY_25"
+        params["alpha_pose"] = 0.6
+        params["scale_gap"] = 0.3
+        params["scale_number"] = 1
+        params["render_threshold"] = 0.05
+        # If GPU version is built, and multiple GPUs are available, set the ID here
+        params["num_gpu_start"] = 0
+        params["disable_blending"] = False
+        # Ensure you point to the correct path where models are located
+        params["model_folder"] = 'C:\\Users\\seanz\\CS_Projects\\openpose\\models'
+        return params
+
+def main():
+        params = set_params()
+
+        #Constructing OpenPose object allocates GPU memory
+        # openpose = OpenPose(params)
+        opWrapper = op.WrapperPython()
+        opWrapper.configure(params)
+        opWrapper.start()
+
+        #Opening OpenCV stream
+        webcam = VideoStream(src=0).start()
+
+        font = cv2.FONT_HERSHEY_SIMPLEX
+
+        while True:
+                
+                # ret, img = stream.read()
+                frame = webcam.read()
+
+                datum = op.Datum()
+                # imageToProcess = cv2.imread(frame)
+                datum.cvInputData = frame
+                opWrapper.emplaceAndPop([datum])
+
+                # Display the stream
+                cv2.putText(frame,'OpenPose using Python-OpenCV',(20,30), font, 1,(255,255,255),1,cv2.LINE_AA)
+
+                cv2.imshow('Human Pose Estimation', datum.cvOutputData)
+
+                key = cv2.waitKey(1)
+
+                if key==ord('q'):
+                        break
+
+        cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main()
+
+
+# trained model method - way to slow
+
 # ap = argparse.ArgumentParser()
 # ap.add_argument("-p", "--prototxt", required=True,
 #                 help="path to Caffe 'deploy' prototxt file")
@@ -17,92 +83,57 @@ import os
 # ap.add_argument("-c", "--confidence", type=float, default=0.5,
 #                 help="minimum probability to filter weak detections")
 # args = vars(ap.parse_args())
-#
-# print("[INFO] loading model...")
+
+# protoFile = "/pose_deploy_linevec_faster_4_stages.prototxt"
+# weightsFile = "/pose_iter_160000.caffemodel"
+# imageFile = "/genji.png"
+# npoints = 15
+# POSE_PAIRS = [[0,1], [1,2], [2,3], [3,4], [1,5], [5,6], [6,7], [1,14], [14,8], [8,9], [9,10], [14,11], [11,12], [12,13] ]
+
+# # net = cv2.dnn.readNetFromCaffe(protoFile, weightsFile)
 # net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
-#
-# # turning on webcam
-# print("[INFO] starting video stream...")
-webcam = VideoStream(src=0).start()
-time.sleep(2.0)
 
+# # this frame can be passed in from camera feed
+# frame = cv2.imread(dir_path + imageFile)
+# frameWidth = frame.shape[1]
+# frameHeight = frame.shape[0]
+# threshold = 0.1
 
-class FrontEnd(object):
+# inWidth = 300
+# inHeight = 300 
 
-    def __init__(self):
-        pygame.init()
-        self.screen = pygame.display.set_mode([960, 720])
+# blob = cv2.dnn.blobFromImage(
+#     cv2.resize(frame, (300, 300)), 1.0 / 255, (inWidth, inHeight), 
+#     (104.0, 177.0, 123.0), swapRB=False, crop=False)
 
-    def run(self):
-        showing = True
-        body_detection = False
+# net.setInput(blob)
+# output = net.forward()
 
-        while showing:
-            frame = webcam.read()
-            cv2.imshow("Webcam", frame)
-            cam = webcam.frame
-            cv2.putText(cam, "body_detection: " + str(body_detection), 
-            (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 255, 255), 2)
+# H = output.shape[2]
+# W = output.shape[3]
+# points = []
 
-            for event in pygame.event.get():
-                if event.type == KEYDOWN:
-                    if event.key == K_ESCAPE:
-                        showing = False
-                    elif event.key == K_b:
-                        if body_detection:
-                            body_detection = False
-                        else:
-                            body_detection = True
-            if not showing:
-                break
+# for i in range(npoints):
+#     probMap = output[0, i, :, :]
+#     minVal, prob, minLoc, point = cv2.minMaxLoc(probMap)
+#     x = (frameWidth * point[0]) / W
+#     y = (frameHeight * point[1]) / H
 
-            # convert each frame into a blob
-            (height, width) = cam.shape[:2]
-            blob = cv2.dnn.blobFromImage(cv2.resize(cam, (300, 300)), 
-                1.0, (300, 300), (104.0, 177.0, 123.0))
+#     if prob > threshold :
+#         cv2.circle(frame, (int(x), int(y)), 5, (0, 255, 255), thickness=-1, lineType=cv2.FILLED)
+#         cv2.putText(frame, "{}".format(i), (int(x), int(y)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 3, lineType=cv2.LINE_AA)
+#         points.append((int(x), int(y)))
+#     else: 
+#         points.append(None)
 
+# for pair in POSE_PAIRS:
+#     partA = pair[0]
+#     partB = pair[1]
 
+#     if points[partA] and points[partB]:
+#         cv2.line(frame, points[partA], points[partB], (0, 255, 255), 2)
+#         cv2.circle(frame, points[partA], 8, (0, 0, 255), thickness=-1, lineType=cv2.FILLED)
 
-def main():
-    frontend = FrontEnd()
-    frontend.run()
-
-
-if __name__ == '__main__':
-    main()
-
-# while True:
-#     frame = webcam.read()
-#     frame = imutils.resize(frame, width=400)
-#
-#     # convert frame into blob
-#     (h, w) = frame.shape[:2]
-#     blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0,
-#                                  (300, 300), (104.0, 177.0, 123.0))
-#
-#     net.setInput(blob)
-#     detections = net.forward()
-#
-#     for i in range(0, detections.shape[2]):
-#         confidence = detections[0, 0, i, 2]
-#         if confidence < args["confidence"]:
-#             continue
-#         # compute bounding box
-#         box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-#         (startX, startY, endX, endY) = box.astype("int")
-#         # draw the bounding box around face
-#         text = "{:.2f}%".format(confidence * 100)
-#         y = startY - 10 if startY - 10 > 10 else startY + 10
-#         cv2.rectangle(frame, (startX, startY), (endX, endY),
-#                       (0, 0, 255), 2)
-#         cv2.putText(frame, text, (startX, y),
-#                     cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
-#
-#         cv2.imshow("Frame", frame)
-#         key = cv2.waitKey(1) & 0xFF
-#
-#         if key == ord("q"):
-#             break
-#
-#     cv2.destroyAllWindows()
-#     webcam.stop()
+# cv2.imshow("Output", frame)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
